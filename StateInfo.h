513eb5ea90e2e19ed75791738c90ff67260c1804
Hanuman Chu <chuhanuman@yahoo.com>
2022-12-17 08:04:06 -0700

    Polished headers

--- StateInfo.h
+++ StateInfo.h
@@ -1,11 +1,4 @@
-/* CSCI 200: Final Project: Ultimate Tic Tac Toe
- *
- * Author: Hanuman Chu
- * Resources used (Office Hours, Tutoring, Other Students, etc & in what capacity):
- *     // list here any outside assistance you used/received while following the
- *     // CS@Mines Collaboration Policy and the Mines Academic Code of Honor
- * 	   Designed By Hugo - youtube.com/watch?v=Kx5CN-V6FvQ - Used to create barebones of UTTTGameWindow class
- *     Surag Nair - web.stanford.edu/~surag/posts/alphazero.html - Used for creating the trainer executable's main function, used to adapt MCTS to be used with neural networks, and used to create the NeuralNetwork and UTTTNet classes and StateInfo struct
+/* Author: Hanuman Chu
  * 
  * Creates StateInfo struct which holds information about a GameState just for use in MCTS
  */
